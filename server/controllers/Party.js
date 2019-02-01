63b87af3c6bbbd78209924e6ec7a3ca8a32a83b9
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-02-01 06:27:06 +0100

    Update Party.js

--- server/controllers/Party.js
+++ server/controllers/Party.js
@@ -15,9 +15,17 @@ const createParty = (req, res) => {
   });
 };
 
-const getAllParties = (req, res) => res.status(200).json({
-  status: 200,
-  data: parties,
+const getAllParties = ((req, res) => {
+  if (parties.length <= 0) {
+    return res.status(404).json({
+      status: 404,
+      error: 'No political party has been created',
+    });
+  }
+  res.status(200).json({
+    status: 200,
+    data: parties,
+  });
 });
 
 const getAParty = (req, res) => {
