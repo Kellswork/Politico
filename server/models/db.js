8f5f110ca5df895330b94e60c0577735ff730e02
kellswork <kellsogbonna@gmail.com>
2019-02-05 18:26:19 +0100

    (chore #feedback): Implement feedback - remove commented out code - add .env.sample file [Delivers #163681940]

--- server/models/db.js
+++ server/models/db.js
@@ -8,11 +8,8 @@ if (process.env.NODE_ENV === 'test') {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL || process.env.LOCALDB_URl,
     ssl: true,
-  }, console.log(process.env.LOCALDB_URL));
+  });
 }
-
-// pool.connect().then(() => console.log('connected to db'))
-//   .catch(err => console.log(err.stack));
 
 const db = {
   query: (text, params) => pool.query(text, params),
