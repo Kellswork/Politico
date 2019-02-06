4325ba1bfdd2d16b2c61935d92219a95140da539
kellswork <kellsogbonna@gmail.com>
2019-02-06 15:54:38 +0100

    fix party table query error

--- server/models/partyQuery.js
+++ server/models/partyQuery.js
@@ -1,5 +1,5 @@
 const createParty = 'INSERT INTO parties(name, hqAddress, logoUrl) VALUES($1,$2,$3) RETURNING *';
 const getParty = 'SELECT * FROM parties LIMIT 6';
-const party = 'SELECT * FROM party WHERE id = $1';
+const party = 'SELECT * FROM parties WHERE id = $1';
 
 export { getParty, createParty, party };
