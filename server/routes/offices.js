4c15478c331b1fee12991b64a8c3f4af1a786e6e
kellswork <kellsogbonna@gmail.com>
2019-02-06 02:47:44 +0100

    (feat create office): Implement create political office - add authentication to route - add admin authorization to route [Delivers #163747008]

--- server/routes/offices.js
+++ server/routes/offices.js
@@ -2,9 +2,11 @@ import express from 'express';
 import validateId from '../middleware/validateId';
 import validateOffice from '../middleware/validateOffice';
 import Office from '../controllers/officeController';
+import auth from '../middleware/authentication';
+import admin from '../middleware/authorization';
 
 const router = express.Router();
 
-router.post('/', validateOffice, Office.createOffice);
+router.post('/', auth, admin, validateOffice, Office.createOffice);
 
 export default router;
