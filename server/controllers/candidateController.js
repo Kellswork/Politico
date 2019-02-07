0c0886784d0e786fe2827f990af9f2e89fd9e1f5
kellswork <kellsogbonna@gmail.com>
2019-02-07 09:34:18 +0100

    (feat vote): Implement user can vote - add api endpoint to let users vote -  validate req.body [Delivers #163682028]

--- server/controllers/candidateController.js
+++ server/controllers/candidateController.js
@@ -31,10 +31,50 @@ class Candidate {
       if (candidateExists.rowCount >= 1) {
         return res.status(406).json({
           status: 406,
-          error: 'user has already been registered for an office',
+          error: 'you cannot regiter a user twice',
         });
       }
       const result = await db.query('INSERT into candidates(officeId, partyId, userId) VALUES($1,$2,$3) RETURNING *', [officeId, partyId, userId]);
+      console.log(result);
+      res.status(201).json({
+        status: 201,
+        data: result.rows[0],
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: 500,
+        error: err.message,
+      });
+    }
+  }
+
+  static async vote(req, res) {
+    try {
+      const { office, candidate } = req.body;
+      const voter = req.userData.id;
+      const checkOffice = await db.query('select * from offices where id = $1', [office]);
+      if (checkOffice.rowCount < 1) {
+        return res.status(404).json({
+          status: 404,
+          error: 'office not found',
+        });
+      }
+      const checkCandidate = await db.query('select * from candidates where id = $1', [candidate]);
+      if (checkCandidate.rowCount < 1) {
+        return res.status(404).json({
+          status: 404,
+          error: 'office not found',
+        });
+      }
+      const checkVote = await db.query('select * fom votes where office=$1 and createdBy= $2', [office, voter]);
+      if (checkVote.rowCount >= 1) {
+        return res.status(406).json({
+          status: 406,
+          error: ' You cannot vote for the same office twice',
+        });
+      }
+
+      const result = await db.query('INSERT into votes(createdBy, officeId, candidateId) VALUES($1,$2,$3) RETURNING *', [voter, office, candidate]);
       console.log(result);
       res.status(201).json({
         status: 201,
