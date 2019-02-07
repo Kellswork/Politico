bb6961d2c125fc3d7c1cd0abeb89a922b283c850
kellswork <kellsogbonna@gmail.com>
2019-02-07 11:15:49 +0100

    (feat election result): Implement get election results - add api endpoint to fetch election results [Delivers #163682036]

--- server/routes/offices.js
+++ server/routes/offices.js
@@ -12,5 +12,6 @@ router.post('/', auth, admin, validateOffice, Office.createOffice);
 router.get('/', auth, Office.getAllOffices);
 router.get('/:id', validateId, auth, Office.getASpecificOffice);
 router.post('/:userId/register', auth, admin, validateCandidate, Candidate.registerCandidate);
+router.get('/:officeId/result', Candidate.electionResult);
 
 export default router;
