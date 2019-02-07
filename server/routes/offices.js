4a936fff09ff0f7338d09b61fd8d089d47c63762
kellswork <kellsogbonna@gmail.com>
2019-02-07 11:21:05 +0100

    add authentication to route

--- server/routes/offices.js
+++ server/routes/offices.js
@@ -12,6 +12,6 @@ router.post('/', auth, admin, validateOffice, Office.createOffice);
 router.get('/', auth, Office.getAllOffices);
 router.get('/:id', validateId, auth, Office.getASpecificOffice);
 router.post('/:userId/register', auth, admin, validateCandidate, Candidate.registerCandidate);
-router.get('/:officeId/result', Candidate.electionResult);
+router.get('/:officeId/result', auth, Candidate.electionResult);
 
 export default router;
